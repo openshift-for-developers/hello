1fbf332b926762f191c95e487fdb8ef2907109c6
Maarten <mp.bekie@gmail.com>
2022-12-09 11:23:29 +0100

    Hello World

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello World!"
 	}
 
 	fmt.Fprintln(w, response)
