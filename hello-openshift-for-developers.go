f978fc0d874a8704901089c07fde10f01655b834
Zelmar Vázquez Vázquez <zelmar@192.168.1.14>
2023-03-12 13:45:05 -0600

    response changed

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World"
+		response = "Hello openshift for developers"
 	}
 
 	fmt.Fprintln(w, response)
