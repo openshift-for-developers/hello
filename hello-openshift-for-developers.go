46c3f3ed97766fd75910436ce34c040fc51c67ac
Andy Repton <10834938+andyrepton@users.noreply.github.com>
2023-10-19 11:41:55 +0100

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Welcome to the Red Hat summit!"
 	}
 
 	fmt.Fprintln(w, response)
